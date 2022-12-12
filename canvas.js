0b343e3a7c940e4e1a83893b7d476fe2967df5aa
Faisal I <mfaisal.ilyas.d@gmail.com>
2022-12-12 21:49:18 +0530

    arcs and rectangles

--- canvas.js
+++ canvas.js
@@ -1,2 +1,11 @@
-var canvas = document.querySelector('canvas')
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
 
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 7;
+ctx.beginPath()
+ctx.arc(200,200,100,0, Math.PI*2)
+ctx.stroke()
