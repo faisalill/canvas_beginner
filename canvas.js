99a99614dffcc11b95ed5f3e06b5f72b10380232
Faisal I <mfaisal.ilyas.d@gmail.com>
2022-12-12 23:26:01 +0530

    brush

--- canvas.js
+++ canvas.js
@@ -1,11 +1,37 @@
-const canvas = document.querySelector('canvas')
-const ctx = canvas.getContext('2d');
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-ctx.fillStyle = 'white'
-ctx.strokeStyle = 'white'
-ctx.lineWidth = 7;
-ctx.beginPath()
-ctx.arc(200,200,100,0, Math.PI*2)
-ctx.stroke()
+window.addEventListener("resize", function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
+
+const mouse = {
+  x: undefined,
+  y: undefined,
+};
+
+canvas.addEventListener("click", function(event){
+  mouse.x = event.x;
+  mouse.y = event.y;
+  drawCircle()
+});
+
+canvas.addEventListener('mousemove', function(event){
+    mouse.x = event.x;
+    mouse.y = event.y;
+    drawCircle()
+})
+     
+
+function drawCircle() {
+  ctx.fillStyle = "white";
+  ctx.strokeStyle = "white";
+  ctx.lineWidth = 7;
+  ctx.beginPath();
+  ctx.arc(mouse.x, mouse.y, 100, 0, Math.PI * 2);
+  ctx.fill();
+}
+drawCircle()
